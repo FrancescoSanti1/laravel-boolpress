d282d2585ac261e872da94c44be8cab9411e81b8
francesco <francescosanti91@gmail.com>
2022-02-04 18:10:53 +0100

    add index page

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{asset('/css/app.css')}}">
+    <title>Home page</title>
+</head>
+<body>
+    <div class="container text-center">
+        <h1>Ciao</h1>
+        <h3>Per vedere i post pubblicati, accedi o registrati.</h3>
+        <div class="btn btn-primary">Accedi</div>
+        <div class="btn btn-primary">Registrati</div>
+    </div>
+
+    <script src="{{asset('/js/app.js')}}"></script>
+</body>
+</html>
